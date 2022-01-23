216b9d58bf1bef28ecb5559a7722c6d571e6b7c6
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-01-23 22:32:36 +0300

    Initial commit

---
+++ src/validations/MatchValuesValidation.js
@@ -0,0 +1,14 @@
+export class MatchValuesValidation {
+	constructor(values, fieldNames) {
+		this.validate(values, fieldNames);
+	}
+
+	validate(value, name) {
+		const isValid = value.length > 0 && value[0] === value[1];
+
+		return {
+			isValid,
+			message: isValid ? undefined : `${name[0]} and ${name[1]} does not match.`,
+		};
+	}
+}
